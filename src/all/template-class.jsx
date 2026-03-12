49782246392c9b9d1beeb514fad8b7d02446a4f2
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-03-12 12:00:38 -0500

    feat: add to template methods

--- src/all/template-class.jsx
+++ src/all/template-class.jsx
@@ -18,7 +18,7 @@ export default class Submodule {
      * @abstract Method run when post results are searched for.
      * @param {string} query Value of input element.
      * @returns {{
-     *  thumbnail: string; preview: string;
+     *  thumbnail: string; preview: string; href: string;
      *  id: string | number; tags: { name: string; count: number; }[];
      * }[]}
      */
@@ -42,19 +42,69 @@ export default class Submodule {
         this.element.input.addEventListener("keydown",
             event => event.key === "Enter" && this.submitSearch()
         );
+        this.element.submit.addEventListener("click",
+            () => this.submitSearch()
+        );
+        this.element.input.addEventListener("input",
+            () => this.suggestAutocompletion()
+        );
     }
 
+    /**
+     * @param {string} id 
+     * @param {string | undefined} query 
+     * @returns {HTMLElement}
+     */
     getElement(id, query) {
         const el = document.getElementById(id);
         if (query) return el.querySelector(query);
         else return el;
     }
 
+    suggestAutocompletion() {
+        this.element.autocomplete.replaceChildren();
+        const query = this.element.input.value;
+
+        Promise.resolve(this.getAutocompletion(query)).then(results =>
+            this.element.autocomplete
+            .replaceChildren(...results.map((tag, index) => (
+                <li key={index}>
+                    <span className="name">{ tag.name }</span>
+                    <span className="count">{ tag.count }</span>
+                </li>
+            )))
+        );
+    }
+
     submitSearch() {
+        this.element.results.replaceChildren();
         const query = this.element.input.value;
 
         this.urlParam.set({ q: query });
+        Promise.resolve(this.getSearchResults(query)).then(results =>
+            this.element.results
+            .replaceChildren(...results.map((post, index) => (
+                <li
+                    key={index}
+                    title={
+                        `${post.id}: ${post.tags
+                            .map(t => `${t.name} (${t.count})`)
+                            .join(", ")
+                        }`
+                    }
+                >
+                    <a href={post.href}>
+                        <img
+                            src={post.thumbnail}
+                            onMouseOver={setSrc(post.preview)}
+                            onMouseOut ={setSrc(post.thumbnail)}
+                        />
+                    </a>
+                </li>
+            )))
+        );
         
+        var setSrc = (src) => function () { this.src = src; };
     }
 
     urlParam = {

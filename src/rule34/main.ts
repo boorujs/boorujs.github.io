31435719cee27e57e9faa05c73242f9cca5a6e28
Belle (Laptop) <nightofthebuttlickers@gmail.com>
2026-03-24 19:35:11 -0500

    fix: use eval to keep comment in bundle

--- src/rule34/main.ts
+++ src/rule34/main.ts
@@ -1,14 +1,18 @@
 import * as Rule34 from "https://esm.sh/gh/boorujs/booru.js@v0.1.0-alpha/src/module/rule34/index.ts.mjs?target=es2022";
 import { Submodule } from "../all/template-class.ts";
 
-/* this page is in testing and i just want to be sure esm.sh works before i get
- * custom api keys or a vercel service set up
- *
- * i dont value an individual limited key enough to hide it in any way, ill
- * refresh the key for the account once i get custom keys set up #lol
- * 
- * regardless dont abuse this key please and thanks!
- */
+window.eval(`\`
+
+this page is in testing and i just want to be sure esm.sh works before i get
+custom api keys or a vercel service set up
+
+i dont value an individual limited key enough to hide it in any way, ill
+refresh the key for the account once i get custom keys set up #lol
+
+regardless dont abuse the given key please and thanks!
+
+\``);
+
 const auth = {
     api_key: "57931764243134609eb715b9d5c931134cb3cadfdbe166548b2b25543b869f82ee435076819810388fac48119b1eb0731dc86d75aff7b163e5d0c01f21618ecb",
     user_id: 5894621
